817f02607ff5e9152fe3384bcd20977dc9e01332
gudals2040 <qd0420@gmail.com>
2025-09-30 12:46:58 +0900

    first commit

---
+++ READ.md
@@ -0,0 +1,5 @@
+```
+# npm -iniy -y : 이건 하실 필요 엇ㅂ고
+# git clone, git pull...
+npm install # package.json
+npm run dev # 30000
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express"); // npm install express
+// tailwind, bootstrap -> 의존성 , 설치.
+const app = express();
+const port = 3000;
+
+// Get -> Fetch, Get/Post
+app.get("/", (req, res) => {
+  // localhost:3000/ -> GET/접속 (브라우저를 통한 접속)
+  // res.send("Hello World");
+  res.send("Bye Earth!");
+});
+
+// listen -> server를 구동. -> localhost: 3000
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
